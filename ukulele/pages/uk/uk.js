7656050c71a27ee9b45329b7f4bbfcb66307b1da
徐智勇 <bostonhsu@outlook.com>
2017-09-21 01:14:28 +0800

    完成ukulele项目的测试和学习。

---
+++ ukulele/pages/uk/uk.js
@@ -0,0 +1,107 @@
+Page({
+    //初始赋值
+    data:{
+        src:'../images/c.png',
+        btnc:'active',
+        btnd:'inactive',
+        btne:'inactive',
+        btnf:'inactive',
+        btng:'inactive',
+        btna:'inactive',
+        btnb:'inactive'
+    },
+
+    //点击按钮C触发事件
+    c:function(){
+        this.setData({
+          src:'../images/c.png',
+          btnc:'active',
+          btnd:'inactive',
+          btne:'inactive',
+          btnf:'inactive',
+          btng:'inactive',
+          btna:'inactive',
+          btnb:'inactive'
+        })
+    },
+
+   d:function(){
+        this.setData({
+          src:'../images/d.png',
+          btnc:'inactive',
+          btnd:'active',
+          btne:'inactive',
+          btnf:'inactive',
+          btng:'inactive',
+          btna:'inactive',
+          btnb:'inactive'
+        })    
+    },
+
+   e:function(){
+        this.setData({
+          src:'../images/e.png',
+          btnc:'inactive',
+          btnd:'inactive',
+          btne:'active',
+          btnf:'inactive',
+          btng:'inactive',
+          btna:'inactive',
+          btnb:'inactive'
+      })    
+    },
+
+   f:function(){
+        this.setData({
+          src:'../images/f.png',
+          btnc:'inactive',
+          btnd:'inactive',
+          btne:'inactive',
+          btnf:'active',
+          btng:'inactive',
+          btna:'inactive',
+          btnb:'inactive'
+      })    
+    },
+    
+   g:function(){
+        this.setData({
+          src:'../images/g.png',
+          btnc:'inactive',
+          btnd:'inactive',
+          btne:'inactive',
+          btnf:'inactive',
+          btng:'active',
+          btna:'inactive',
+          btnb:'inactive'
+      })    
+    },
+
+    
+   a:function(){
+        this.setData({
+          src:'../images/a.png',
+          btnc:'inactive',
+          btnd:'inactive',
+          btne:'inactive',
+          btnf:'inactive',
+          btng:'inactive',
+          btna:'active',
+          btnb:'inactive'
+      })    
+    },
+
+    
+   b:function(){
+        this.setData({
+          src:'../images/b.png',
+          btnc:'inactive',
+          btnd:'inactive',
+          btne:'inactive',
+          btnf:'inactive',
+          btng:'inactive',
+          btna:'inactive',
+          btnb:'active'
+      })    
+    }
+})
