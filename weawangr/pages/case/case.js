1106fd20ec00895ebe1d9cecca06a432a3ee28fd
Boston Hsu <bostonhsu@outlook.com>
2017-09-26 13:45:58 +0800

    完成第8章的学习和实验。

---
+++ weawangr/pages/case/case.js
@@ -0,0 +1,9 @@
+Page({
+  data: {
+    imgUrls: [
+      '../images/banner1.jpg',
+      '../images/banner2.jpg',
+      '../images/banner3.jpg',
+    ],
+  }
+})
---
+++ weawangr/pages/map/map.js
@@ -0,0 +1,12 @@
+// pages/map/map.js
+Page({
+  data: {
+    markers: [{
+      iconPath: "../images/mark.png",
+      id: 0,
+      latitude: 39.294255,
+      longitude: 117.796789,
+      title: '荣亨集团',
+    }],
+  }
+})
